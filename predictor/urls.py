3c839c1d0ce166b67e8786eb48262c5cd0c87d34
Abdur Rahman <abdur.rahman@juspay.in>
2017-05-13 00:30:43 +0530

    created route

--- predictor/urls.py
+++ predictor/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^hit/$', views.dummy_json, name='index')
 ]
--- predictor/views.py
+++ predictor/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, JsonResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def dummy_json(request):
+    return JsonResponse({"lat":12.20,"lot":12.23})
