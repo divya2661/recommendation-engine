3de00d84d594a820e136f728fffb2e83df8139ac
divya2661 <divn2661@gmail.com>
2017-05-13 22:36:44 +0530

    added json

--- predictor/views.py
+++ predictor/views.py
@@ -11,4 +11,32 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def dummy_json(request):
-    return JsonResponse({"lat":12.20,"lot":12.23})
+    return JsonResponse({
+  "items": [
+    {
+      "name": "Koramangla 4th block",
+      "lat": 12.9354381,
+      "lon": 77.5617222
+    },
+    {
+      "name": "Koramangla 5th block",
+      "lat": 12.9560138,
+      "lon": 77.5916371
+    },
+    {
+      "name": "Koramangla 6th block",
+      "lat": 12.9560126,
+      "lon": 77.5915512
+    },
+    {
+      "name": "Koramangla 7th block",
+      "lat": 12.9317898,
+      "lon": 77.629813
+    },
+    {
+      "name": "Koramangla 8th block",
+      "lat": 12.9422533,
+      "lon": 77.6203564
+    }
+  ]
+})
